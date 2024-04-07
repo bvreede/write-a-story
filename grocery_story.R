5b79e4f9ade64ebf1c0f7c3c349648d5721dc1cd
Barbara Vreede <b.m.i.vreede@umcutrecht.nl>
2024-04-07 21:08:58 +0200

    Create grocery_story.R

---
+++ grocery_story.R
@@ -0,0 +1,22 @@
+## collect the variables, and pick one at random
+
+people <- c("Jennifer", "Albert", "Tiago", "Oisin", "Miriam", "Linda", "Emmy", "Daniel")
+person <- sample(people, 1)
+
+fruits <- c("banana", "melon", "mango", "grape", "apple", "cucumber", "papaya")
+fruit <- sample(fruits, 1)
+
+items <- c("toilet paper", "wine", "trash bags", "shampoo", "dog food")
+item <- sample(items, 1)
+
+actions <- c("smashed", "shredded", "opened", "crushed", "squeezed")
+verb <- sample(actions, 1)
+
+transportation <- c("ufo", "flying carpet", "ATV", "roller skates", "lawn mower")
+transportmode <- sample(transportation, 1)
+
+judgings <- c("fabulous", "questionable", "top notch", "interesting", "wonderful")
+judgement <- sample(judgings, 1)
+
+# Make the story
+madlibs::story_grocer(person, fruit, item, verb, transportmode, judgement)
