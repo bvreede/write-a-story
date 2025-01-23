c27db3bfd771c2b227f23867f2097a80dab33d3d
Barbara Vreede <barbara@datalab.nl>
2025-01-23 15:30:29 +0100

    add story and use it in test (#18)

---
+++ stories.R
@@ -0,0 +1,11 @@
+# Make the story
+story <- madlibs::story_grocer(
+    firstname = "Sam",
+    fruit = "papaya",
+    grocery_item = "butter",
+    verb = "ate",
+    mode_of_transport = "unicycle",
+    judgement = "confusing"
+)
+
+cat(story)
